ecbcac3713a4110862c73ad0ed24d752587d4fa1
Eduardo <eduardoarevaloportero@gmail.com>
2025-02-10 16:41:03 +0100

    commit de app.js Revisad que no falte ninguna de vuestras rutas

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+require('./database');
+require('./passport/local-auth');
+
+var usersRouter = require('./routes/users');
+var studiesRouter = require('./routes/studies');
+var subjectsRouter = require('./routes/subjects');
+var softwaresRouter = require('./routes/softwares');
 
 // view engine setup
+app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -17,10 +25,35 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use("/public", express.static(path.resolve(__dirname + '/public')));
+app.use(session({
+  secret: 'mysecretsession',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/subjects', subjectsRouter);
+app.use('/studies', studiesRouter);
+app.use('/softwares', softwaresRouter);
+
+app.use((req, res, next) => {
+  app.locals.signinMessage = req.flash('signinMessage');
+  app.locals.user = req.user;
+  next();
+});
+
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+app.use('/', subjectsRouter);
+app.use('/', studiesRouter);
+app.use('/', softwaresRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
