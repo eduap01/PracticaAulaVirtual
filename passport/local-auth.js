c7625fc5af3a80dbb4d785794e630c87b02bb726
Eduardo <eduardoarevaloportero@gmail.com>
2025-02-11 17:39:41 +0100

    commit correcto de local-auth.js

--- passport/local-auth.js
+++ passport/local-auth.js
@@ -1,28 +1,31 @@
-<% include partials/_header %>
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-<div class="container">
-  <div class="row">
-    <!--FORM-->
-    <div class="col-md-5 offset-md-3">
-      <div class="card">
-        <div class="card-body">
-          <form action="/tasks/edit/<%= task._id %>" method="post">
-            <div class="form-group">
-              <input class="form-control" type="text" name="title" placeholder="Title" value="<%= subject.title %>">
-            </div>
-            <div class="form-group">
-              <textarea class="form-control" name="description" cols="80"
-                placeholder="Add a Description"
-                ><%= subject.description %></textarea>
-            </div>
-            <button class="btn btn-primary" type="submit">
-              Update
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
+const User = require('../models/user');
 
-<% include partials/_footer%>
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  done(null, user);
+});
+
+//no hay sign-up
+
+passport.use('local-signin', new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, async (req, email, password, done) => {
+  var user = new User();
+   user = await user.findEmail( email);
+  if(!user) {
+    return done(null, false, req.flash('signinMessage', 'No User Found'));
+  }
+  if(!user.comparePassword(password)) {
+    return done(null, false, req.flash('signinMessage', 'Incorrect Password'));
+  }
+  return done(null, user);
+}));
